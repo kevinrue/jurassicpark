db52a4f97bb647d774f8a40edba8f5dd71fbd8c1
Kevin Rue <kevinrue@users.noreply.github.com>
2024-09-01 10:06:54 +0100

    forgot to commit the python script

---
+++ tests/scvelo/ubuntu-latest.py
@@ -0,0 +1,17 @@
+import scvelo as scv
+import scanpy as sc
+
+scv.settings.verbosity = 3  # show errors(0), warnings(1), info(2), hints(3)
+scv.settings.presenter_view = True  # set max width size for presenter view
+scv.set_figure_params('scvelo')  # for beautified visualization
+
+adata = scv.datasets.pancreas()
+
+scv.pp.filter_genes(adata, min_shared_counts=20)
+scv.pp.normalize_per_cell(adata)
+scv.pp.filter_genes_dispersion(adata, n_top_genes=2000)
+scv.pp.log1p(adata)
+
+scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+sc.pp.neighbors(adata, n_pcs=30, n_neighbors=30)
+scv.pp.moments(adata, n_pcs=None, n_neighbors=None)
