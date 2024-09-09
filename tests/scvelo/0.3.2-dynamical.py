01eb270fe4239dbf7d213931a9ae1872c9921c6a
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-09-09 11:37:18 +0100

    Fix steps that involve multiprocessing (#5)
    * test dynamical mode on macos
    
    * try installing loky 3.4.1
    
    * add all python steps
    
    * display environment before running code (in case of fail)
    
    * 1 core
    
    * backend = multiprocessing
    
    * suppress progress bar
    
    * suppress progress bar
    
    * njobs=1
    
    * add velocity steps for windows
    
    * install tqdm ipywidgets
    
    * try https://github.com/theislab/scvelo/issues/746#issuecomment-974679256
    
    * remove n_jobs

--- tests/scvelo/0.3.2-dynamical.py
+++ tests/scvelo/0.3.2-dynamical.py
@@ -1,4 +1,5 @@
 import scvelo as scv
+import scanpy as sc
 
 scv.settings.verbosity = 3  # show errors(0), warnings(1), info(2), hints(3)
 scv.settings.presenter_view = True  # set max width size for presenter view
@@ -13,10 +14,15 @@ if __name__ == '__main__':
 	scv.pp.log1p(adata)
 	
 	scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
-	scv.pp.moments(adata, n_pcs=30, n_neighbors=30)
+	sc.pp.neighbors(adata, n_pcs=30, n_neighbors=30)
+	scv.pp.moments(adata, n_pcs=None, n_neighbors=None)
 	
-	scv.tl.velocity(adata)
-	scv.tl.velocity_graph(adata, n_jobs=1)
+	scv.tl.recover_dynamics(adata)
+	
+	scv.tl.velocity(adata, mode='dynamical')
+	scv.tl.velocity_graph(adata)
 	scv.tl.velocity_pseudotime(adata)
+	
+	scv.tl.latent_time(adata)
 	
 	scv.tl.velocity_confidence(adata)
