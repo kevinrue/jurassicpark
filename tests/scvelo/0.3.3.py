422032c174b5fb9e13f3a57a1f6c68a58d54c474
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2026-04-15 10:27:30 +0100

    update macos environment and test script

---
+++ tests/scvelo/0.3.3.py
@@ -0,0 +1,31 @@
+# Based on https://scvelo.readthedocs.io/en/stable/getting_started.html
+
+import scvelo as scv
+import scanpy as sc
+
+scv.settings.verbosity = 3  # show errors(0), warnings(1), info(2), hints(3)
+scv.settings.presenter_view = True  # set max width size for presenter view
+scv.set_figure_params('scvelo')  # for beautified visualization
+
+if __name__ == '__main__':
+	adata = scv.datasets.pancreas()
+	
+	# scv.pp.filter_genes(adata, min_shared_counts=20)
+	# scv.pp.normalize_per_cell(adata)
+	scv.pp.filter_and_normalize(adata, min_shared_counts=20)
+	# scv.pp.filter_genes_dispersion(adata, n_top_genes=2000)
+	# scv.pp.log1p(adata)
+	
+	# scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+	sc.pp.neighbors(adata, n_pcs=30, n_neighbors=30)
+	scv.pp.moments(adata, n_pcs=None, n_neighbors=None)
+	
+	# to test dynamical mode
+	# scv.tl.recover_dynamics()
+	
+	# default mode = 'steady_state'
+	scv.tl.velocity(adata, mode='steady_state')
+	scv.tl.velocity_graph(adata, n_jobs=1)
+	scv.tl.velocity_pseudotime(adata)
+	
+	scv.tl.velocity_confidence(adata)
